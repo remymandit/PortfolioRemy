dc2c5f6ca1426569a7a275b0c0909ea5cbcb2834
Mandit <remy.mandit@free.fr>
2013-12-21 16:12:47 +0100

    Use Apc
    Add ApcClassLoader in app.php

--- web/app.php
+++ web/app.php
@@ -1,20 +1,18 @@
 <?php
 // Point d'entrée de l'application
-
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-
-
+$apcLoader = new ApcClassLoader('portfolioremy_sf2', $loader);
+$loader->unregister();
+$apcLoader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 
-
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-
-
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
